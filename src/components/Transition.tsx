00f0421bc9ed318ecb94dbef8da143dae9028d69
Yudai35 <yuusongben14@gmail.com>
2023-03-11 17:37:15 +0900

    sec5 Suspenseを学ぶ準備（useQuery）

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -34,6 +34,7 @@ const filteringAssignee = (assignee: string) => {
 export const Transition = () => {
     const [selectedAssignee, setSelectedAssignee] = useState<string>('');
     const [taskList, setTaskList] = useState<Task[]>(tasks);
+    const [isShowList, setIsShowList] = useState<boolean>(false);
 
     const onClickAssignee = (assignee: string) => {
         setSelectedAssignee(assignee); 
@@ -50,7 +51,10 @@ export const Transition = () => {
             </div>
             <br />
             <button onClick={() => onClickAssignee('')}>リセット</button>
-            <TaskList taskList={taskList} />
+            <br />
+            <br />
+            <button onClick={() => setIsShowList(!isShowList)}>表示/非表示</button>
+            {isShowList && <TaskList taskList={taskList} />}
         </div>
     )
 }
